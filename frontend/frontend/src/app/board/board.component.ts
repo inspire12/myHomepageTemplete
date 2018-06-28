24f5e6e8a7ed5e9ca67e424f104c7036b30f1392
yeonghakseo <ox4443@naver.com>
2018-06-29 01:06:13 +0900

    paginator 프론트 작업

--- frontend/frontend/src/app/board/board.component.ts
+++ frontend/frontend/src/app/board/board.component.ts
@@ -14,12 +14,31 @@ export class BoardComponent implements OnInit {
   public contentData: object;
   public depth: number[][];
 
+  // 한페이지에 얼마나 보여주는지
+  public showPage: number;
+  public ngPageNumber: number[];
+  // 지금 페이지
+  public nowPage: number;
+  // 보여주는 글 부분
+  public showContent: string[];
+  // 전체 글 데이터를 받는 부분
+  public allContent: string[];
+
+  public totalPage: number;
+
   constructor(private http: HttpClient) {
     this.baseUrl = 'http://127.0.0.1:5001/';
+    this.ngPageNumber = [];
+    this.showPage = 10;
+    this.nowPage = 8;
+    this.totalPage = 0;
   }
 
   ngOnInit() {
-    this.loadContentList();
+    // this.loadContentList();
+    // totalPage = totalArticle / pageView;
+
+    this.setPagenate();
   }
 
   loadContentList(): void {
@@ -36,10 +55,47 @@ export class BoardComponent implements OnInit {
           console.log(data[index]['_id']);
         }
       }
-      this.contentData = Object.values(data);
-
-      console.log(this.contentData);
+      this.allContent = Object.values(data);
+      this.totalPage = this.allContent.length / this.showPage ;
     });
   }
 
+  public setPagenate() {
+    this.ngPageNumber = [];
+    if (this.nowPage > this.totalPage) {
+      return;
+    }
+    if (this.nowPage < this.showPage / 2) {
+      for (let i = 0; i < this.showPage; i++) {
+        this.ngPageNumber.push(i);
+      }
+    } else if (this.nowPage + this.showPage / 2 > this.totalPage) {
+      for (let i = this.totalPage - this.showPage; i < this.totalPage; i++) {
+        this.ngPageNumber.push(i);
+      }
+    } else {
+      for (let i = this.nowPage - Math.floor((this.showPage) / 2); i < this.nowPage + Math.ceil((this.showPage) / 2); i++) {
+        this.ngPageNumber.push(i);
+      }
+    }
+    console.log(this.ngPageNumber);
+    this.showContent = [];
+    for (let i = this.nowPage * this.showPage; i < this.allContent.length && i < (this.nowPage + 1) * this.showPage; i++) {
+      this.showContent.push(this.allContent[i]);
+    }
+  }
+
+  public selectPage(element: number): boolean {
+    return (element === this.nowPage);
+  }
+
+  public clickPage(element: number): void {
+    this.nowPage = element;
+    this.setPagenate();
+
+
+
+
+    // 내용 바꿔주는 부분
+  }
 }
